8a954a5969de8cdd1eead3bcd85d0301990b660c
Anuragjuneja <anuragjuneja01@gmail.com>
2024-06-14 11:43:14 +0530

    Update StreamLit1.py

--- StreamLit1.py
+++ StreamLit1.py
@@ -17,7 +17,7 @@ st.markdown("<h2 style='color:white;'>Upload an image to determine if it is<br>A
 page_bg_img = '''
 <style>
 .stApp {
-  background-image: url("https://i.pinimg.com/originals/5c/81/cb/5c81cb143b0e627d7500bdea99d53409.jpg");
+  background-image: url("https://ibb.co/YhZRXmb");
   background-size: cover;
 }
 </style>
